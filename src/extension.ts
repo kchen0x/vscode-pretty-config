f6ad365160d15e40058c84651184501f6f9662db
KChen <kchen2991@gmail.com>
2018-09-28 20:05:01 +0800

    first extension example of markdown wordcount

--- src/extension.ts
+++ src/extension.ts
@@ -11,17 +11,74 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "vscode-pretty-config" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let wordCounter = new WordCounter();
+    let controller = new WordCounterController(wordCounter);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    context.subscriptions.push(wordCounter);
+    context.subscriptions.push(controller);
+}
 
-    context.subscriptions.push(disposable);
+class WordCounter {
+    constructor() {
+        
+    }
+
+    private _statusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+
+    /**
+     * updateWordCount
+     */
+    public updateWordCount() {
+        
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+        if (doc.languageId === "markdown") {
+            let wordCount = this._getWordCount(doc);
+            this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : '1 Word';
+            this._statusBarItem.show();
+        } else {
+            this._statusBarItem.hide();
+        }
+    }
+
+    private _getWordCount(doc: vscode.TextDocument) {
+        let docContent = doc.getText();
+        let wordCount = 0;
+        if (docContent !== "") {
+            wordCount = docContent.split(" ").length;
+        }
+        return wordCount;
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
+}
+
+class WordCounterController {
+    private _wordCounter: WordCounter;
+    private _disposable: vscode.Disposable;
+    constructor(wordCounter: WordCounter) {
+        this._wordCounter = wordCounter;
+
+        let subscriptions: vscode.Disposable[] = [];
+        vscode.window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        vscode.window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+
+        this._wordCounter.updateWordCount();
+        this._disposable = vscode.Disposable.from(...subscriptions);
+    }
+    dispose() {
+        this._disposable.dispose();
+    }
+    private _onEvent() {
+        this._wordCounter.updateWordCount();
+    }
 }
 
 // this method is called when your extension is deactivated
